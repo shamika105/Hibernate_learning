05e4402291417e323b2800524431ce7d891d6bac
Shamika_Diwan <91006618+shamika105@users.noreply.github.com>
2024-04-17 08:32:43 +0530

    hello
    This one file contain POJO Class file(Plain Old Java Object) & another file is tes file.

---
+++ ProductTest.java
@@ -0,0 +1,34 @@
+package com.pojo;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.pojo.Product;
+
+
+public class ProductTest {
+	  
+	public static void main(String[] args) {
+		Configuration cfg=new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		
+		SessionFactory sf= cfg.buildSessionFactory();
+		Session s=sf.openSession();
+		 
+		Transaction tx=s.beginTransaction();
+		
+		Product p=new Product();
+		
+//		to insert record into table
+		p.setPid(101);
+		p.setPname("Mobile");
+		p.setPrice(20000);
+		s.save(p);
+		tx.commit();
+		s.close();
+		
+		
+	}
+
+}
